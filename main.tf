b33909ab6380c048a699019a537ebe0cda49f904
RossSingleton <ross-singleton@hotmail.co.uk>
2020-08-28 15:03:45 +0100

    Code now deploys a VPC, subnet, Security group, and EC2 instance.

--- main.tf
+++ main.tf
@@ -1,12 +1,42 @@
 provider "aws" {
   region = "eu-west-1"
+  profile = "devopsguys-sandbox"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "Ross-Test-VPC"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "Ross-Test-Subnet"
+    }
+}
+
+resource "aws_security_group" "secgroup" {
+  name = "ross-test-security-group"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-07ee42ba0209b6d77"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["vpc-0589f9e1393afcf96"]
-  subnet_id = "subnet-04d13f14b77d3115d"
+  vpc_security_group_ids = [aws_security_group.secgroup.id]
+  subnet_id = aws_subnet.subnet.id
 
   tags = {
     Name = "Ross Test Instance"
