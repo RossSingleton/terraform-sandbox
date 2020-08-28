d4fe2c7d7b964d2e93cd7c7c155c55aec191b784
RossSingleton <ross-singleton@hotmail.co.uk>
2020-08-28 15:27:58 +0100

    The code now also makes use of remote state storage, although the bucket and key have been removed for security

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+    backend "s3" {
+        bucket = ""
+        key    = ""
+        region = "eu-west-1"
+        profile = "devopsguys-sandbox"
+    }
+}
+
 provider "aws" {
   region = "eu-west-1"
   profile = "devopsguys-sandbox"
