f3fe855adf985a4c9336a67d550e6de01c346277
RossSingleton <ross-singleton@hotmail.co.uk>
2020-09-28 15:31:13 +0100

    Added beginning structure of the Vsphere VM module

---
+++ vsphere-test/variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {
+    description = "The user to access VSphere"
+    type = string
+}
+
+variable "vsphere_password" {
+    description = "The password to access VSphere"
+    type = string
+}
+
+variable "vsphere_server" {
+    description = "The server to access VSphere"
+    type = string
+}
